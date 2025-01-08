8611b18a859114d4e392287ad0d99a9ab1e30886
Jörn Röder <kontakt@joernroeder.de>
2025-01-08 12:20:00 +0100

    build: enable graph optimization

--- metro.config.js
+++ metro.config.js
@@ -6,4 +6,17 @@ const { withNativeWind } = require('nativewind/metro');
 // eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
 
+/**
+ * Enable experimental treeshaking
+ * @see https://docs.expo.dev/guides/tree-shaking/
+ */
+process.env.EXPO_UNSTABLE_METRO_OPTIMIZE_GRAPH = 1; // <-- Turning this on breaks the styles.
+process.env.EXPO_UNSTABLE_TREE_SHAKING = 0; // <-- haven't tried to turn this on too.
+
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: true,
+  },
+});
+
 module.exports = withNativeWind(config, { input: './global.css' });
